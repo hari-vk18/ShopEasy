a4e831ff7916542583c98ae5de6bcd47cf70cc23
hari-vk18 <prasathhari972@gmail.com>
2024-12-23 13:05:54 +0530

    restrict permission

--- ShopEasy/src/main/java/com/project/ShopEasy/Controller/CartController.java
+++ ShopEasy/src/main/java/com/project/ShopEasy/Controller/CartController.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ public class CartController {
 	@Autowired
 	private ICartService cartService;
 
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping("/{cartId}/my-cart")
 	public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
 		try {
@@ -34,6 +36,7 @@ public class CartController {
 		}
 	}
 
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@DeleteMapping("/{cartId}/clear")
 	public ResponseEntity<ApiResponse> clearCart(@PathVariable Long cartId) {
 		try {
@@ -44,6 +47,7 @@ public class CartController {
 		}
 	}
 
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping("/{cartId}/cart/total-price")
 	public ResponseEntity<ApiResponse> getTotalPrice(@PathVariable Long cartId) {
 		try {
