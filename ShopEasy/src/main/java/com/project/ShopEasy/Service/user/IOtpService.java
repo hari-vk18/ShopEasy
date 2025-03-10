269067a71b99ab01c1530c1e1167f9989f0a3c33
hari-vk18 <prasathhari972@gmail.com>
2025-03-10 18:18:33 +0530

    otp

---
+++ ShopEasy/src/main/java/com/project/ShopEasy/Service/user/IOtpService.java
@@ -0,0 +1,11 @@
+package com.project.ShopEasy.Service.user;
+
+public interface IOtpService {
+
+	String generateOtp(String email);
+
+	boolean verifyOtp(String email, String otp);
+
+	void sendOtptoEmail(String email, String otp);
+
+}
