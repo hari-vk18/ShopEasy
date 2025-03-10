c290e3d68b1cfee28755b2ac4d48c5e0ecc923d9
hari-vk18 <prasathhari972@gmail.com>
2025-03-10 18:19:54 +0530

    Rate limit

--- ShopEasy/src/main/java/com/project/ShopEasy/Secutity/Config/ShopEasyConfig.java
+++ ShopEasy/src/main/java/com/project/ShopEasy/Secutity/Config/ShopEasyConfig.java
@@ -1,11 +1,15 @@
 package com.project.ShopEasy.Secutity.Config;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -19,6 +23,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.project.ShopEasy.Secutity.RateLimit.RateLimitingFilter;
 import com.project.ShopEasy.Secutity.User.ShopUserDetailsService;
 import com.project.ShopEasy.Secutity.jwt.AuthTokenFilter;
 import com.project.ShopEasy.Secutity.jwt.JwtAuthEntryPoint;
@@ -33,6 +38,12 @@ public class ShopEasyConfig {
 
 	@Autowired
 	private JwtAuthEntryPoint authEntryPoint;
+
+	@Value("${spring.mail.username}")
+	private String mailUsername;
+
+	@Value("${spring.mail.password}")
+	private String mailPassword;
 
 	private static final List<String> SECURED_URLS = List.of("/api/v1/carts/**", "/api/v1/cartItems/**",
 			"/api/v1/orders/**");
@@ -50,6 +61,11 @@ public class ShopEasyConfig {
 	@Bean
 	public AuthTokenFilter authTokenFilter() {
 		return new AuthTokenFilter();
+	}
+
+	@Bean
+	public RateLimitingFilter rateLimitingFilter() {
+		return new RateLimitingFilter();
 	}
 
 	@Bean
@@ -75,8 +91,26 @@ public class ShopEasyConfig {
 				.authorizeHttpRequests(auth -> auth.requestMatchers(SECURED_URLS.toArray(String[]::new)).authenticated()
 						.anyRequest().permitAll());
 		http.authenticationProvider(daoAuthenticationProvider());
+		http.addFilterBefore(rateLimitingFilter(), UsernamePasswordAuthenticationFilter.class);
 		http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 
+	}
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername(mailUsername);
+		mailSender.setPassword(mailPassword);
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
 	}
 }
